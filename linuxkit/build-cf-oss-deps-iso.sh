de446f0eb1c6735a93ab9aa82f6b73300be6af7a
Dave Protasowski <dprotaso@gmail.com>
2018-01-26 15:25:04 -0500

    creating the cf iso process now caches any releases it compiles
    Signed-off-by: Steve Hiehn <shiehn@pivotal.io>

--- linuxkit/build-cf-oss-deps-iso.sh
+++ linuxkit/build-cf-oss-deps-iso.sh
@@ -13,21 +13,16 @@ function is_release() {
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 download_dir="${script_dir}/download-cache"
 download_list="${download_dir}/downloads.txt"
+compiled_dir="${script_dir}/compiled-cache"
 iso_file="${script_dir}/cf-oss-deps.iso"
 
 rm -rf "${iso_file}"
 rm -rf "${download_list}"
 
-
-if [ -z "$SKIP_PURGE" ]; then
-  rm -rf "${download_dir}"
-fi
-
-mkdir -p "${download_dir}"
+mkdir -p "${download_dir}" "${compiled_dir}"
 touch "${download_list}"
 
-"${script_dir}/../images/cf-oss/build.sh"
-
+# Collect download items
 manifest_bosh=$(docker run pivotal/cf-oss cat /etc/bosh/director.yml)
 stemcell_url=$(echo "${manifest_bosh}" | bosh int - --path /resource_pools/name=vms/stemcell/url)
 #stemcell_version="$(echo "${stemcell_url}" | grep -o 'v=\(\d*\.\d*\)' | cut -d '=' -f 2)"
@@ -39,27 +34,40 @@ stemcell_version=$(echo "$manifest_cf" | bosh interpolate - --path /stemcells/0/
 echo "$manifest_cf" | bosh int - --path /releases | grep url | awk '{print $2}' >> "${download_list}"
 echo "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=$stemcell_version" >> "${download_list}"
 
+iso_dir=$(mktemp -d)
+
+# Place the 'workspace' container image
+"${script_dir}/../images/cf-oss/build.sh"
 cid=$(docker run -d pivotal/cf-oss sleep infinity)
-docker export "$cid" > "${download_dir}/workspace.tar"
+docker export "$cid" > "${iso_dir}/workspace.tar"
 docker kill "$cid"
 docker rm "$cid"
 
 wget --no-http-keep-alive -P "${download_dir}" -c -i "${download_list}"
 
-iso_dir=$(mktemp -d)
+for url in $(cat "${download_list}"); do
+ file="${download_dir}/$(basename $url)"
 
-for f in "${download_dir}/"*; do
- if ! is_release "$f"; then
-   cp "${f}" "${iso_dir}"
+ if ! is_release "${file}"; then
+   cp "${file}" "${iso_dir}"
    continue
  fi
 
- if ! has_uncompiled_packages "$f"; then
-   cp "${f}" "${iso_dir}"
+ # Already compiled
+ if ! has_uncompiled_packages "${file}"; then
+   cp "${file}" "${iso_dir}"
    continue
  fi
 
- "${script_dir}/compile-release.sh" "${f}" "${stemcell_version}" "${iso_dir}/$(basename ${f})"
+ compiled_release="${compiled_dir}/$(basename $url)"
+
+ if [ ! -f "${compiled_release}" ]; then
+   "${script_dir}/compile-release.sh" "${file}" "${stemcell_version}" "${compiled_release}"
+ fi
+
+ cp "${compiled_release}" "${iso_dir}"
+
 done
+
 
 mkisofs -V cf-oss-deps -R -o "${iso_file}" "${iso_dir}"
