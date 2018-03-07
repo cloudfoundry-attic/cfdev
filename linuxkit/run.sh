4e328a528207d44dbe5f20fd6dbaebc7d42ec8d8
Emily Casey <ecasey@pivotal.io>
2018-03-07 09:57:43 -0500

    Add CI Support

--- linuxkit/run.sh
+++ linuxkit/run.sh
@@ -2,26 +2,45 @@
 
 # This still requires Docker For Mac
 
-set -e
+set -ex
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+while getopts "i:c:" arg; do
+  case $arg in
+    i) image="$OPTARG"
+      ;;
+    c) cf_deps_iso="$OPTARG"
+      ;;
+  esac
+done
+
+cache_dir="$HOME"/.cfdev/cache
+script_dir="$( cd "$(dirname "$0")" && pwd)"
+
+if [[ -z $image ]]; then
+    image="$cache_dir"/cfdev-efi.iso
+fi
+if [[ -z $cf_deps_iso ]]; then
+    cf_deps_iso="$cache_dir"/cf-oss-deps.iso
+fi
+
 
 rm -rf $script_dir/cfdev-efi-state
 
-linuxkit_bin="$script_dir/linuxkit"
-hyperkit_bin="$script_dir/hyperkit"
-vpnkit_bin="$script_dir/vpnkit"
-qcowtool_bin="$script_dir/qcow-tool"
-uefi_fw="$script_dir/UEFI.fd"
+linuxkit_bin="$cache_dir/linuxkit"
+hyperkit_bin="$cache_dir/hyperkit"
+vpnkit_bin="$cache_dir/vpnkit"
+qcowtool_bin="$cache_dir/qcow-tool"
+uefi_fw="$cache_dir/UEFI.fd"
 
 $linuxkit_bin run hyperkit \
     -console-file \
-    -hyperkit $hyperkit_bin \
+    -state "$script_dir"/cfdev-efi-state \
+    -hyperkit "$hyperkit_bin" \
 	-cpus 4 \
 	-mem 8192 \
-	-fw $uefi_fw \
+	-fw "$uefi_fw" \
 	-networking vpnkit \
-	-vpnkit $vpnkit_bin \
+	-vpnkit "$vpnkit_bin" \
 	-disk type=qcow,size=50G,trim=true,qcow-tool=$qcowtool_bin,qcow-onflush=os,qcow-compactafter=262144,qcow-keeperased=262144 \
-	-disk file=cf-oss-deps.iso \
-	--uefi cfdev-efi.iso
+	-disk file="$cf_deps_iso" \
+	--uefi "$image"
