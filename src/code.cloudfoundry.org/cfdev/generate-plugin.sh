72d7468df3d9b95a65e8d280b3760c4cfcfda86e
Emily Casey <ecasey@pivotal.io>
2018-03-20 15:32:39 -0400

    Moves all scripts to scripts dir, uses executables from the cache dir

--- src/code.cloudfoundry.org/cfdev/generate-plugin.sh
+++ src/code.cloudfoundry.org/cfdev/generate-plugin.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-cfdev="/Users/pivotal/workspace/cfdev"
+set -ex
 
-export GOPATH=$cfdev
+cfdev="/Users/pivotal/workspace/cfdev"
+dir="$( cd "$( dirname "$0" )" && pwd )"
+cfdev="$dir"/../../..
+cache_dir="$HOME"/.cfdev/cache
+
+export GOPATH="$cfdev"
 pkg="code.cloudfoundry.org/cfdev/config"
 
 export GOOS=darwin
@@ -11,19 +16,19 @@ export GOARCH=amd64
 go build \
   -ldflags \
     "-X $pkg.cfdepsUrl=file://$cfdev/output/cf-oss-deps.iso
-     -X $pkg.cfdepsMd5=$(md5 ${cfdev}/output/cf-oss-deps.iso | awk '{ print $4 }')
+     -X $pkg.cfdepsMd5=$(md5 $cfdev/output/cf-oss-deps.iso | awk '{ print $4 }')
      -X $pkg.cfdevefiUrl=file://$cfdev/output/cfdev-efi.iso
-     -X $pkg.cfdevefiMd5=$(md5 ${cfdev}/output/cfdev-efi.iso | awk '{ print $4 }')
-     -X $pkg.vpnkitUrl=file://$cfdev/linuxkit/vpnkit
-     -X $pkg.vpnkitMd5=$(md5 ${cfdev}/linuxkit/vpnkit | awk '{ print $4 }')
-     -X $pkg.hyperkitUrl=file://$cfdev/linuxkit/hyperkit
-     -X $pkg.hyperkitMd5=$(md5 ${cfdev}/linuxkit/hyperkit | awk '{ print $4 }')
-     -X $pkg.linuxkitUrl=file://$cfdev/linuxkit/linuxkit
-     -X $pkg.linuxkitMd5=$(md5 ${cfdev}/linuxkit/linuxkit | awk '{ print $4 }')
-     -X $pkg.qcowtoolUrl=file://$cfdev/linuxkit/qcow-tool
-     -X $pkg.qcowtoolMd5=$(md5 ${cfdev}/linuxkit/qcow-tool | awk '{ print $4 }')
-     -X $pkg.uefiUrl=file://$cfdev/linuxkit/UEFI.fd
-     -X $pkg.uefiMd5=$(md5 ${cfdev}/linuxkit/UEFI.fd | awk '{ print $4 }')" \
+     -X $pkg.cfdevefiMd5=$(md5 $cfdev/output/cfdev-efi.iso | awk '{ print $4 }')
+     -X $pkg.vpnkitUrl=file://$cache_dir/vpnkit
+     -X $pkg.vpnkitMd5=$(md5 "$cache_dir"/vpnkit | awk '{ print $4 }')
+     -X $pkg.hyperkitUrl=file://$cache_dir/hyperkit
+     -X $pkg.hyperkitMd5=$(md5 "$cache_dir"/hyperkit | awk '{ print $4 }')
+     -X $pkg.linuxkitUrl=file://$cache_dir/linuxkit
+     -X $pkg.linuxkitMd5=$(md5 "$cache_dir"/linuxkit | awk '{ print $4 }')
+     -X $pkg.qcowtoolUrl=file://$cache_dir/qcow-tool
+     -X $pkg.qcowtoolMd5=$(md5 "$cache_dir"/qcow-tool | awk '{ print $4 }')
+     -X $pkg.uefiUrl=file://$cache_dir/UEFI.fd
+     -X $pkg.uefiMd5=$(md5 "$cache_dir"/UEFI.fd | awk '{ print $4 }')" \
      code.cloudfoundry.org/cfdev
 
 
