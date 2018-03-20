c5034ff106e8bcc2d4a6e820cc9366ddab49cdd4
Emily Casey <ecasey@pivotal.io>
2018-03-20 14:13:03 -0400

    Removes unused kernel build scripts & patches

--- images/kernel/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-docker build \
-    --build-arg KERNEL_VERSION=4.9.78 \
-    --build-arg KERNEL_SERIES=4.9.x \
-    -t cfdev/kernel:4.9.78 \
-    "$DIR"
