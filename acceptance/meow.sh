7f68baab5ac086155017511a555741b87a1f912e
Dave Goddard <dave@goddard.id.au>
2018-05-18 12:19:08 -0400

    Add flakeAttempts to cats runs
    CATs regularly fails in CI

--- acceptance/meow.sh
+++ acceptance/meow.sh
@@ -44,7 +44,7 @@ function run_cats() {
 EOF
 
   pushd $script_dir/src/github.com/cloudfoundry/cf-acceptance-tests >/dev/null
-    GOPATH=$script_dir ./bin/test -slowSpecThreshold=120 ${@:2} .
+    GOPATH=$script_dir ./bin/test -slowSpecThreshold=120 --flakeAttempts=3 ${@:2} .
   popd >/dev/null
 
 }
