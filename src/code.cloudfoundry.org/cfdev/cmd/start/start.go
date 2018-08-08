aa05a2c0e2118c9e93ff31d828369c97b232eea4
Emily Casey <ecasey@pivotal.io>
2018-08-08 13:49:33 -0400

    refactor stage 1 - dedup indentical funcs
    [#159236302]

--- src/code.cloudfoundry.org/cfdev/cmd/start/start.go
+++ src/code.cloudfoundry.org/cfdev/cmd/start/start.go
@@ -5,13 +5,17 @@ import (
 
 	"code.cloudfoundry.org/cfdev/iso"
 
-	"os"
-
 	"code.cloudfoundry.org/cfdev/config"
 	"code.cloudfoundry.org/cfdev/errors"
 	"code.cloudfoundry.org/cfdev/garden"
 	"code.cloudfoundry.org/cfdev/process"
 	"code.cloudfoundry.org/cfdev/resource"
+	"github.com/spf13/cobra"
+	"time"
+	"strings"
+	"net/url"
+	"fmt"
+	"os"
 )
 
 //go:generate mockgen -package mocks -destination mocks/ui.go code.cloudfoundry.org/cfdev/cmd/start UI
@@ -108,7 +112,68 @@ type Start struct {
 	GardenClient    GardenClient
 }
 
-func CleanupStateDir(cfg config.Config) error {
+const compatibilityVersion = "v1"
+const defaultMemory = 4192
+
+func (s *Start) Cmd() *cobra.Command {
+	args := Args{}
+	cmd := &cobra.Command{
+		Use: "start",
+		RunE: func(_ *cobra.Command, _ []string) error {
+			if err := s.Execute(args); err != nil {
+				return errors.SafeWrap(err, "cf dev start")
+			}
+			return nil
+		},
+	}
+
+	pf := cmd.PersistentFlags()
+	pf.StringVarP(&args.DepsIsoPath, "file", "f", "", "path to .dev file containing bosh & cf bits")
+	pf.StringVarP(&args.Registries, "registries", "r", "", "docker registries that skip ssl validation - ie. host:port,host2:port2")
+	pf.IntVarP(&args.Cpus, "cpus", "c", 4, "cpus to allocate to vm")
+	pf.IntVarP(&args.Mem, "memory", "m", 0, "memory to allocate to vm in MB")
+	pf.BoolVarP(&args.NoProvision, "no-provision", "n", false, "start vm but do not provision")
+
+	pf.MarkHidden("no-provision")
+	return cmd
+}
+
+func (s *Start) waitForGarden() {
+	for {
+		if err := s.GardenClient.Ping(); err == nil {
+			return
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+
+func (s *Start) parseDockerRegistriesFlag(flag string) ([]string, error) {
+	if flag == "" {
+		return nil, nil
+	}
+
+	values := strings.Split(flag, ",")
+
+	registries := make([]string, 0, len(values))
+
+	for _, value := range values {
+		// Including the // will cause url.Parse to validate 'value' as a host:port
+		u, err := url.Parse("//" + value)
+
+		if err != nil {
+			// Grab the more succinct error message
+			if urlErr, ok := err.(*url.Error); ok {
+				err = urlErr.Err
+			}
+			return nil, fmt.Errorf("'%v' - %v", value, err)
+		}
+		registries = append(registries, u.Host)
+	}
+	return registries, nil
+}
+
+func cleanupStateDir(cfg config.Config) error {
 	for _, dir := range []string{cfg.StateDir, cfg.VpnKitStateDir} {
 		if err := os.RemoveAll(dir); err != nil {
 			return errors.SafeWrap(err, "Unable to clean up .cfdev state directory")
--- src/code.cloudfoundry.org/cfdev/cmd/start/start_windows.go
+++ src/code.cloudfoundry.org/cfdev/cmd/start/start_windows.go
@@ -3,45 +3,14 @@ package start
 import (
 	"fmt"
 	"html/template"
-	"net/url"
 	"os"
 	"path/filepath"
-	"strings"
-	"time"
-
 	"code.cloudfoundry.org/cfdev/cfanalytics"
 	"code.cloudfoundry.org/cfdev/env"
 	"code.cloudfoundry.org/cfdev/errors"
 	"code.cloudfoundry.org/cfdev/process"
 	"code.cloudfoundry.org/cfdev/resource"
-	"github.com/spf13/cobra"
 )
-
-const compatibilityVersion = "v1"
-const defaultMemory = 4192
-
-func (s *Start) Cmd() *cobra.Command {
-	args := Args{}
-	cmd := &cobra.Command{
-		Use: "start",
-		RunE: func(_ *cobra.Command, _ []string) error {
-			if err := s.Execute(args); err != nil {
-				return errors.SafeWrap(err, "cf dev start")
-			}
-			return nil
-		},
-	}
-
-	pf := cmd.PersistentFlags()
-	pf.StringVarP(&args.DepsIsoPath, "file", "f", "", "path to .dev file containing bosh & cf bits")
-	pf.StringVarP(&args.Registries, "registries", "r", "", "docker registries that skip ssl validation - ie. host:port,host2:port2")
-	pf.IntVarP(&args.Cpus, "cpus", "c", 4, "cpus to allocate to vm")
-	pf.IntVarP(&args.Mem, "memory", "m", 0, "memory to allocate to vm in MB")
-	pf.BoolVarP(&args.NoProvision, "no-provision", "n", false, "start vm but do not provision")
-
-	pf.MarkHidden("no-provision")
-	return cmd
-}
 
 func (s *Start) Execute(args Args) error {
 	go func() {
@@ -84,7 +53,7 @@ func (s *Start) Execute(args Args) error {
 		return errors.SafeWrap(err, "environment setup")
 	}
 
-	if err := CleanupStateDir(s.Config); err != nil {
+	if err := cleanupStateDir(s.Config); err != nil {
 		return errors.SafeWrap(err, "cleaning state directory")
 	}
 
@@ -172,38 +141,4 @@ func (s *Start) Execute(args Args) error {
 	s.Analytics.Event(cfanalytics.START_END)
 
 	return nil
-}
-
-func (s *Start) waitForGarden() {
-	for {
-		if err := s.GardenClient.Ping(); err == nil {
-			return
-		}
-		time.Sleep(time.Second)
-	}
-}
-
-func (s *Start) parseDockerRegistriesFlag(flag string) ([]string, error) {
-	if flag == "" {
-		return nil, nil
-	}
-
-	values := strings.Split(flag, ",")
-
-	registries := make([]string, 0, len(values))
-
-	for _, value := range values {
-		// Including the // will cause url.Parse to validate 'value' as a host:port
-		u, err := url.Parse("//" + value)
-
-		if err != nil {
-			// Grab the more succinct error message
-			if urlErr, ok := err.(*url.Error); ok {
-				err = urlErr.Err
-			}
-			return nil, fmt.Errorf("'%v' - %v", value, err)
-		}
-		registries = append(registries, u.Host)
-	}
-	return registries, nil
 }
