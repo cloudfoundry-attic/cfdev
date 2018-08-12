fa4973a34340d4b2071f9c2ba91fe04b1a938138
Emily Casey <emilycasey@Emilys-MacBook-Pro-2.local>
2018-08-12 18:31:32 -0400

    Fix bad iso path test to be os agnostic

--- src/code.cloudfoundry.org/cfdev/cmd/start/start_test.go
+++ src/code.cloudfoundry.org/cfdev/cmd/start/start_test.go
@@ -303,7 +303,7 @@ var _ = Describe("Start", func() {
 					Cpus:        7,
 					Mem:         6666,
 					DepsIsoPath: "/wrong-path-to-some-deps.iso",
-				})).To(MatchError("no file found at: /wrong-path-to-some-deps.iso"))
+				}).Error()).To(ContainSubstring("no file found"))
 			})
 		})
 
