010546cdf70a3eea932c11f42c5159d03f47877c
Steve Hiehn <shiehn@pivotal.io>
2018-09-24 13:38:05 -0400

    Store whitelisted services in a config

---
+++ analyticsd/config/service_whitelist.go
@@ -0,0 +1,9 @@
+package config
+
+var (
+	SERVICE_WHITELIST = []string{
+		"mysql", "p-mysql", "p.mysql",
+		"rabbit", "rabbitmq", "p-rabbitmq", "p.rabbitmq",
+		"redis", "p-redis", "p.redis",
+	}
+)
