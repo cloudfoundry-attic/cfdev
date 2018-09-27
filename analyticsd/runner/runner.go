81e2d217a55e2c93136d849ccf221bb43c37b4a9
Andy Voellmer <avoellmer@pivotal.io>
2018-09-27 16:07:57 -0400

    Remove unreferenced runner package
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- analyticsd/runner/runner.go
+++
@@ -1,9 +0,0 @@
-package runner
-
-import "os/exec"
-
-type Runner struct {}
-
-func (r *Runner) Output(command string, arg ...string) ([]byte, error) {
-	return exec.Command(command, arg...).Output()
-}
