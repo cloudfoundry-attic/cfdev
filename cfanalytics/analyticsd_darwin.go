2c1dc1256750c0d2b9f166e694ce26f8254dd897
Anthony Emengo <aemengo@pivotal.io>
2018-10-01 15:26:02 -0400

    Fixes "CFDEV_DEBUG" not being passed on to analyticsd on darwin

--- cfanalytics/analyticsd_darwin.go
+++ cfanalytics/analyticsd_darwin.go
@@ -12,7 +12,7 @@ func (a *AnalyticsD) DaemonSpec() daemon.DaemonSpec {
 		Label:            AnalyticsDLabel,
 		Program:          filepath.Join(a.Config.CacheDir, "analyticsd"),
 		SessionType:      "Background",
-		ProgramArguments: []string{os.Getenv("CFDEV_MODE")},
+		ProgramArguments: []string{filepath.Join(a.Config.CacheDir, "analyticsd"), os.Getenv("CFDEV_MODE")},
 		RunAtLoad:        false,
 		StdoutPath:       path.Join(a.Config.CFDevHome, "analyticsd.stdout.log"),
 		StderrPath:       path.Join(a.Config.CFDevHome, "analyticsd.stderr.log"),
