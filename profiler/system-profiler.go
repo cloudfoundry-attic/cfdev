e3b75acf54b91f7e673b3136aa30a93e3e597671
Steve Hiehn <shiehn@pivotal.io>
2018-10-02 15:16:42 -0400

    Implement SystemProfiler

--- profiler/system-profiler.go
+++ profiler/system-profiler.go
@@ -1,8 +1,15 @@
 package profiler
 
+import "github.com/shirou/gopsutil/mem"
+
 type SystemProfiler struct {
 }
 
-func (*SystemProfiler)GetAvailableMemory() (int, error){
-	return 0, nil
+func (*SystemProfiler)GetAvailableMemory() (uint64, error){
+	virtualMemory, err := mem.VirtualMemory()
+	if err != nil {
+		return 0, nil
+	}
+
+	return virtualMemory.Free/1000000, nil
 }
