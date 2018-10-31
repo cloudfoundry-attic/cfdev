777e6f08bfdf5e7233ba61e89b13b2272e0d5107
Steve Hiehn <shiehn@pivotal.io>
2018-10-31 16:25:14 -0400

    recursively move the services dir

--- env/env.go
+++ env/env.go
@@ -4,6 +4,7 @@ import (
 	"code.cloudfoundry.org/cfdev/resource"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -120,6 +121,11 @@ func SetupState(config config.Config) error {
 		return err
 	}
 
+	err = copyDir(filepath.Join(config.CacheDir, "services"), filepath.Join(config.CFDevHome, "services"))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -147,4 +153,59 @@ func moveFile(srcDir, targetDir string) error {
 	}
 
 	return err
+}
+
+func copyDir(src string, dst string) (err error) {
+	src = filepath.Clean(src)
+	dst = filepath.Clean(dst)
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	if !si.IsDir() {
+		return fmt.Errorf("source is not a directory")
+	}
+
+	_, err = os.Stat(dst)
+	if err != nil && !os.IsNotExist(err) {
+		return
+	}
+	if err == nil {
+		return fmt.Errorf("destination already exists")
+	}
+
+	err = os.MkdirAll(dst, si.Mode())
+	if err != nil {
+		return
+	}
+
+	entries, err := ioutil.ReadDir(src)
+	if err != nil {
+		return
+	}
+
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		dstPath := filepath.Join(dst, entry.Name())
+
+		if entry.IsDir() {
+			err = copyDir(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		} else {
+			// Skip symlinks.
+			if entry.Mode()&os.ModeSymlink != 0 {
+				continue
+			}
+
+			err = moveFile(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
 }
