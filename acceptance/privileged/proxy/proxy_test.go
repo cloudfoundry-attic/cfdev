09ecf2e10565c4faffb68b6f9139483a027a2c9d
Joao Pereira <jdealmeidapereira@pivotal.io>
2018-11-28 15:08:26 -0500

    Comment the HTTPS Proxy test
     * we are commenting out this step in the test because
    https://example.com as a bad certificate, and while it is not
    fixed our test would be failing.
    
    Revert this commit as soon as the problem is corrected

--- acceptance/privileged/proxy/proxy_test.go
+++ acceptance/privileged/proxy/proxy_test.go
@@ -36,9 +36,11 @@ var _ = Describe("cf dev proxy settings", func() {
 			Expect(httpGet("http://cf-test-app.dev.cfdev.sh/external")).To(ContainSubstring("Example Domain"))
 			Eventually(fetchProxyLogs(proxyName)).Should(gbytes.Say(`Established connection to host ".*"`))
 
-			By("making HTTPS requests")
-			Expect(httpGet("http://cf-test-app.dev.cfdev.sh/external_https")).To(ContainSubstring("Example Domain"))
-			Eventually(fetchProxyLogs(proxyName)).Should(gbytes.Say(`CONNECT .*:443 HTTP/1.1`))
+
+			// Certificate in https://example.com site is broken, uncomment when that is no longer the case
+			//By("making HTTPS requests")
+			//Expect(httpGet("http://cf-test-app.dev.cfdev.sh/external_https")).To(ContainSubstring("Example Domain"))
+			//Eventually(fetchProxyLogs(proxyName)).Should(gbytes.Say(`CONNECT .*:443 HTTP/1.1`))
 
 			By("making a request from a site in the NO_PROXY list")
 			Expect(httpGet("http://cf-test-app.dev.cfdev.sh/external_no_proxy")).To(ContainSubstring("www.google.com"))
@@ -59,9 +61,10 @@ var _ = Describe("cf dev proxy settings", func() {
 			boshCurl("http://example.com")
 			Eventually(fetchProxyLogs(proxyName)).Should(gbytes.Say(`Established connection to host ".*"`))
 
-			By("making HTTPS requests")
-			boshCurl("https://example.com")
-			Eventually(fetchProxyLogs(proxyName)).Should(gbytes.Say(`CONNECT .*:443 HTTP/1.1`))
+			// Certificate in https://example.com site is broken, uncomment when that is no longer the case
+			//By("making HTTPS requests")
+			//boshCurl("https://example.com")
+			//Eventually(fetchProxyLogs(proxyName)).Should(gbytes.Say(`CONNECT .*:443 HTTP/1.1`))
 
 			By("making a request from a site in the NO_PROXY list")
 			boshCurl("http://google.com")
